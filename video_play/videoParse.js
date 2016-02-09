fcdd18d70746c06a626c62f46f4d6cb36b730f7c
laurasher <asherlk1@mac.com>
2016-02-09 14:12:31 -0800

    Trying out ffmpeg node module

--- video_play/videoParse.js
+++ video_play/videoParse.js
@@ -1,6 +1,25 @@
 console.log("Video parse:");
 
-// print process.argv
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
+// print file name
+var args = process.argv.slice(2).toString();
+console.log(args);
+
+// ffmpeg -i ./test.3gp
+// -acodec libfaac -ab 128k -ar 41000
+// -vcodec libx264 -vpre slow -vpre baseline -s 640x360 -r 25
+// ./test.mp4
+
+var ffmpeg = require('ffmpeg');
+
+try {
+    new ffmpeg( args, function (err, video) {
+        if (!err) {
+            console.log('The video is ready to be processed');
+        } else {
+            console.log('Error: ' + err);
+        }
+    });
+} catch (e) {
+    console.log(e.code);
+    console.log(e.msg);
+}
