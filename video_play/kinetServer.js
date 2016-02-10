47dc73654ee5534826fadac781ffe29f47755296
laurasher <asherlk1@mac.com>
2016-02-09 17:04:32 -0800

    Server code added

---
+++ video_play/kinetServer.js
@@ -0,0 +1,87 @@
+// - - - - - - - - - - - - - - -
+// KiNet Server
+//
+// Connects to KiNet power supply via UDP
+// Creates KiNet data packet
+// Note: UDP must use port 6038 to communicate with KiNet!
+
+module.exports = {
+
+	createKinetServer : function() { 
+
+		// Require UDP module
+		var dgram = require('dgram');
+		
+		console.log("Creating KiNet Lighting server!");
+
+		var kinetServer = {};
+		kinetServer.socket = dgram.createSocket('udp4');
+
+		// Creates properly formatted header data
+		// TODO: Could expand this for variable strand length
+		// Currently defaults to 50 nodes
+		var getHeaderData = function( iPort ){
+
+		var header = [ 0x04, 0x01, 0xDC, 0x4A,  // magic number
+	    0x02, 0x00,                       // KiNet version
+	    0x08, 0x01,                       // Packet type
+	    0x00, 0x00, 0x00, 0x00,
+	    0xFF, 0xFF, 0xFF, 0xFF, 			// universe, FFFF FFFF is "don't care"
+	    iPort,                             // Port on device controller -- TODO: port will need to be set when using sPDS-480-24V
+	    0x00,                             // pad, unused
+	    0x01, 0x00,                       // Flags, originally 0x01,0x00, 04 00 for sync
+	    150, 0x00,                  	// Set length, setting to 150 (send all values)
+	    0x00, 0x00];
+
+		return header;
+		
+		}
+
+
+		kinetServer.sendKinetData = function( iPixelData, ipAddress, iKinetPort ){
+
+			// If pixel data is too long, clip array
+			if ( iPixelData.length > 150 ){
+
+				console.log("Warning: Pixel data array is too long.  Clipping to 150 pixel values.");
+				iPixelData = iPixelData.slice(0, 150);
+
+			// If pixel data is too short, pad array
+			}else if ( iPixelData.length < 150 ){
+
+				console.log("Warning: Pixel data array is too short.  Padding to 150 pixel values.");
+
+				var padding = 150 - iPixelData.length;
+				var paddingArray = new Array( padding + 1).join('0').split('').map(parseFloat);
+
+				iPixelData = iPixelData.concat( paddingArray );
+			}
+
+			// Compose and send KiNet data packet
+			var writeData = function( iPort, iPayload ) {
+
+				var header = getHeaderData( iPort );
+
+				// Combine KiNet header data with payload
+				var dataPacket = header.concat( iPayload );
+				var message = new Buffer( dataPacket );
+
+
+				// Note: KiNet data MUST be sent over port 6038!
+				kinetServer.socket.send( message, 0, message.length, 6038, ipAddress, function(err) {
+
+					if (err){
+
+						kinetServer.socket.close();
+						throw err;
+					} 
+				});
+			};
+
+			writeData( iKinetPort, iPixelData );
+	}
+
+		return kinetServer;
+
+	}
+};
--- video_play/videoParse.js
+++ video_play/videoParse.js
@@ -1,7 +1,24 @@
+// - - - - - - - - - - - - - - - - -
+// - Parse a video from command line
+// - Get pixel data
+// - Send out rgb values to server
+// - - - - - - - - - - - - - - - - -
+
+//====================
+// SETUP
+//====================
+
 var ffmpeg = require('fluent-ffmpeg');
 var getPixels = require('get-pixels');
+// Require kinetServer module
+var kinetServer = require('./kinetServer.js');
 
-// print file name
+
+//====================
+// Print file name
+//====================
+
+
 var args = process.argv.slice(2).toString();
 console.log(args);
 
@@ -21,9 +38,11 @@ var proc = ffmpeg(args)
   // take 2 screenshots at predefined timemarks
   .takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '6'] }, '/Users/lasher/Sosolimited/PiPlayer/video_play/exported');
 
-// get pixel data from one frame
 
-// write out rgb values to light server
+//====================
+// Get pixel data
+//====================
+
 	getPixels('/Users/lasher/Sosolimited/PiPlayer/video_play/exported/tn_1.png', function(err, pixels){
 		if (err){
 			console.log(err);
@@ -31,4 +50,63 @@ var proc = ffmpeg(args)
 		} else {
 			console.log(pixels.data);
 		}
+	});
+
+
+//====================
+// Send out rgb values
+//====================
+
+	// first check for config.js, which needs to be created manually
+	// at each location (base it on config-default.js)
+	var fs = require('fs');
+
+	if( !fs.existsSync('./config.js') ){
+		console.log( "\nError: config.js not found! Copy config-default.js to config.js and change values as needed.\n" );
+		return;
+	}
+
+	var config = require('./config.js');
+
+	var client = {};
+
+	// Create KiNet server for sending data to lights
+	client.kinetServer = kinetServer.createKinetServer();
+
+	// Connect to local running server instance
+	// TODO: Update to use remote server after deployment
+	client.socket = require('socket.io-client')(config.server, {query: 'location='+config.location});
+	client.frameCount = 0;
+
+	// Letting us know when we connect.
+	client.socket.on('connect', function(){
+		console.log("Connected to Frame Server");
+	});
+
+	// Letting us know when we disconnect.
+	client.socket.on('disconnect', function(){
+		console.log("Disconnected from Frame Server");
+	});
+
+	// On the frame event make a write out.
+	client.socket.on('frame', function(data) {
+
+		var rows = data.height;
+		var cols = data.width;
+		var lightStrand = data.pixels;
+
+		// Format lightstrand for either Boston or San Diego
+		if(config.location==="BOS"){
+			lightStrand = bostonLightConfig(rows, cols, lightStrand).slice();
+
+			client.kinetServer.sendKinetData( lightStrand.slice(0,150), config.kinetIP, 3);
+			client.kinetServer.sendKinetData( lightStrand.slice(150,300), config.kinetIP, 1 );
+			client.kinetServer.sendKinetData( lightStrand.slice(300,lightStrand.length), config.kinetIP, 2);
+
+		}else{
+			lightStrand = SDLightConfig(rows, cols, lightStrand.slice(0,144)).slice();
+
+			client.kinetServer.sendKinetData( lightStrand, config.kinetIP, 1 );
+		}
+
 	});
