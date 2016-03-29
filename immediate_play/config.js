b0498a377fe1b6f397770215204e605ed1f0e1a9
Laura Asher <pi@raspberrypi.(none)>
2016-03-29 20:27:13 +0000

    config still listening to lightpost.sosolimited.com, but works

--- immediate_play/config.js
+++ immediate_play/config.js
@@ -1,4 +1,3 @@
 module.exports.location = "SAN"; //Location as BOS or SAN
-// module.exports.kinetIP = "10.32.30.169";
 module.exports.kinetIP = "10.4.97.107";
 module.exports.server = "http://localhost:3000";
