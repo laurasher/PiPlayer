76ac20281efc80cf059f515d9315f535d279bdba
laurasher <asherlk1@mac.com>
2016-03-30 14:53:56 -0700

    Change startup to work with child process

--- immediate_play/startup.sh
+++ immediate_play/startup.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/bash
 # Start my node project
-# gnome-terminal --tab -e "bash -c 'cd ~/PiPlayer/server && node server.js'" --tab -e "bash -c 'cd ~/PiPlayer/immediate_play && node immediate_play.js'"
-# cd ~/PiPlayer/server && node server.js
-# cd ~/PiPlayer/immediate_play && node immediate_play.js
-
-
-tmux -L list-session
-tmux send-keys 'cd ~/PiPlayer/server' 'enter'
-tmux send-keys 'node server.js'
-tmux
-tmux send-keys 'cd ~/PiPlayer/immediate_play' 'enter'
-tmux send-keys 'node immediate_play.js'
+cd ~/PiPlayer/immediate_play && node immediate_play.js
